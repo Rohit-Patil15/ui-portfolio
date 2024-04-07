e878d8838eabe3ec12bc97e7724f1fa9e48e2a95
Rohit-Patil15 <148066806+Rohit-Patil15@users.noreply.github.com>
2024-04-07 22:42:51 +0530

    version-11
    new changes in resume

---
+++ Bharatiya-unity/bhartiya-unity.js
@@ -0,0 +1,26 @@
+/* -- TYPING EFFECT -- */
+var typingEffect = new Typed(".typedText", {
+    strings: ["Designer", "UI Developer", "Problem Solver"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
+
+
+/* -- SCROLL REVEAL ANIMATION -- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/* -- TYPING EFFECT -- */
+var typingEffect = new Typed(".typedText", {
+    strings: ["Designer", "UI Developer", "Problem Solver"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000
+})
