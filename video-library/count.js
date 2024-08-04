8fd396b0b610bb1ce43899e53c12b00b45abcf57
Rohit-Patil15 <148066806+Rohit-Patil15@users.noreply.github.com>
2024-08-04 06:29:47 +0530

    version-36 updated

---
+++ video-library/count.js
@@ -0,0 +1,29 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Aug 25, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+const countdownfunction = setInterval(() => {
+    // Get today's date and time
+    const now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    const distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Output the result in an element with id="days", "hours", "minutes", "seconds"
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the count down is over, write some text
+    if (distance < 0) {
+        clearInterval(countdownfunction);
+        document.getElementById("timer").innerHTML = "EXPIRED";
+    }
+}, 1000);
